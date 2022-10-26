bad35f9f79463fe9da8989341572d176585b03b8
Pavel Repin <prepin@kontur.io>
2022-10-26 16:21:24 +0400

    Fix for temp table

--- src/main/java/io/kontur/insightsapi/repository/IndicatorRepository.java
+++ src/main/java/io/kontur/insightsapi/repository/IndicatorRepository.java
@@ -70,7 +70,7 @@ public class IndicatorRepository {
         logger.info("IndicatorRepository: uploadCSVFileIntoTempTable");
         String tempTableName = generateTempTableName();
 
-        String tempTableQuery = String.format("CREATE TEMPORARY TABLE %s (h3 h3index, value double precision)", tempTableName);
+        String tempTableQuery = String.format("CREATE TEMPORARY TABLE %s (h3 h3index, value double precision) ON COMMIT PRESERVE ROWS", tempTableName);
         jdbcTemplate.update(tempTableQuery);
 
         logger.info("TEMP table is created with the name " + tempTableName);

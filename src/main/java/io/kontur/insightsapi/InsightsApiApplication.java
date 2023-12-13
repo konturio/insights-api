7815f905b07d3ba33f1fc8919c19c6a29d3a4adc
Alexander Kolesen <alexander.kolesen@gmail.com>
2023-12-13 13:39:17 +0100

    Sentry integration

--- src/main/java/io/kontur/insightsapi/InsightsApiApplication.java
+++ src/main/java/io/kontur/insightsapi/InsightsApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
+import io.sentry.Sentry;
 
 @SpringBootApplication
 @EnableAsync

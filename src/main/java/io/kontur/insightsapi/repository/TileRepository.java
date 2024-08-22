780597522f7ae09f5a819b67f0f2dd9958954c10
sleeping <hh.sleeping@gmail.com>
2024-08-22 14:55:59 +0400

    revert tile generator to use indicator_uuid,h3 index

--- src/main/java/io/kontur/insightsapi/repository/TileRepository.java
+++ src/main/java/io/kontur/insightsapi/repository/TileRepository.java
@@ -100,7 +100,7 @@ public class TileRepository {
             List<String> columns = Lists.newArrayList();
 
             for (BivariateIndicatorDto indicator : bivariateIndicatorDtos) {
-                outerFilter.add(String.format("('%s'::uuid)", indicator.getInternalId()));
+                outerFilter.add(String.format("'%s'", indicator.getInternalId()));
                 columns.add(String.format("coalesce(avg(indicator_value) filter (where indicator_uuid = '%s'), 0) as \"%s\"",
                         indicator.getInternalId(), indicator.getId()));
             }

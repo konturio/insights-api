4f23ed19d055d274fd001f92a2e8e06717a6c7c4
Darafei Praliaskouski <me@komzpa.net>
2025-09-03 21:29:42 +0300

    fix: PROD-INSIGHTS-API-26 support external servlet container

--- src/main/java/io/kontur/insightsapi/InsightsApiApplication.java
+++ src/main/java/io/kontur/insightsapi/InsightsApiApplication.java
@@ -2,13 +2,23 @@ package io.kontur.insightsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @EnableRetry
-public class InsightsApiApplication {
+public class InsightsApiApplication extends SpringBootServletInitializer {
+
+    /**
+     * Allow running the application inside an external servlet container.
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InsightsApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InsightsApiApplication.class, args);
